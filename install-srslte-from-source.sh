4e665e371e3c2218c4bcab8db1ecea9761e70b40
Steven Van Acker <steven@singularity.be>
2021-09-20 12:03:17 +0200

    add pcscd support for hard usim

--- install-srslte-from-source.sh
+++ install-srslte-from-source.sh
@@ -8,6 +8,9 @@ apt-get update && apt-get install -y build-essential git mdm vim nmap tcpdump ip
 # Following https://github.com/srsLTE/srsLTE
 apt-get install --no-install-recommends -y cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev libuhd-dev uhd-host
 
+#  https://docs.srsran.com/en/latest/usermanuals/source/srsue/source/4_ue_advanced.html#external-usim
+apt-get install --no-install-recommends -y libpcsclite-dev pcscd pcsc-tools
+
 mkdir -p /opt
 cd /opt
 git clone https://github.com/srsLTE/srsLTE
